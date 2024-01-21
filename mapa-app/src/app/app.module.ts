33e911454a2d871414d802d68713797a61753779
Cristian <creature.secret@gmail.com>
2024-01-21 16:43:47 -0300

    add maps module

--- mapa-app/src/app/app.module.ts
+++ mapa-app/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MapsModule } from './maps/maps.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MapsModule
   ],
   providers: [
     provideClientHydration()
