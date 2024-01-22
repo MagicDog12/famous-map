550b275d2f6ff08a4fc28d57dbadf2c8cf30f149
Cristian <creature.secret@gmail.com>
2024-01-22 00:08:48 -0300

    add userLocation to map-view

--- mapa-app/src/app/maps/components/map-view/map-view.component.ts
+++ mapa-app/src/app/maps/components/map-view/map-view.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
   styleUrl: './map-view.component.css'
 })
-export class MapViewComponent {
+export class MapViewComponent implements OnInit {
+
+  constructor(
+    private placesService: PlacesService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    console.log(this.placesService.userLocation);
+  }
 
 }
