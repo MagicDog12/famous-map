4f94e3e2ba652bb0fc07cc009939938fc71cfb1f
Cristian <creature.secret@gmail.com>
2024-01-22 00:23:19 -0300

    add map-box to main

--- mapa-app/src/main.ts
+++ mapa-app/src/main.ts
@@ -1,8 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import mapboxgl from 'mapbox-gl';
 
-if(!navigator.geolocation) {
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3Jpc3RpYW4wMTIiLCJhIjoiY2xyb2NubWE2MWpsMDJqa2k3M3NzMGhsbyJ9.LO9_4zw5Rv7nP3f_IA6t2A';
+
+
+if (!navigator.geolocation) {
   alert('Navegador no soporta la geolocation');
   throw new Error('Navegador no soporta la geolocation');
 }
