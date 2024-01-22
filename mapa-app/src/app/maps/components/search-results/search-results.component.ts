a2573762e8f671bd49802990d6944c05c9350cb2
Cristian <creature.secret@gmail.com>
2024-01-22 13:01:13 -0300

    add places results

--- mapa-app/src/app/maps/components/search-results/search-results.component.ts
+++ mapa-app/src/app/maps/components/search-results/search-results.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MapService, PlacesService } from '../../services';
+import { Feature } from '../../interfaces/places';
 
 @Component({
   selector: 'app-search-results',
@@ -7,4 +9,25 @@ import { Component } from '@angular/core';
 })
 export class SearchResultsComponent {
 
+  public selectedId: string = '';
+
+  constructor(
+    private placesService: PlacesService,
+    private mapService: MapService
+  ) {
+  }
+
+  get isLoadingPlaces(): boolean {
+    return this.placesService.isLoadingPlaces;
+  }
+
+  get places(): Feature[] {
+    return this.placesService.places;
+  }
+
+  flyTo(place: Feature) {
+    this.selectedId = place.id;
+    const [lng, lat] = place.center;
+    this.mapService.flyTo([lng, lat], 14);
+  }
 }
