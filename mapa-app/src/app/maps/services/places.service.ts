8d96489a6a9a9b40c4b91ecb2f7b7270922a6099
Cristian <creature.secret@gmail.com>
2024-01-22 12:24:45 -0300

    add placesApi

--- mapa-app/src/app/maps/services/places.service.ts
+++ mapa-app/src/app/maps/services/places.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Feature, PlacesResponse } from '../interfaces/places';
+import { PlacesApiClient } from '../api';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +17,7 @@ export class PlacesService {
   }
 
   constructor(
-    private http: HttpClient,
+    private placesApi: PlacesApiClient,
   ) {
   }
 
@@ -42,8 +43,13 @@ export class PlacesService {
 
   getPlacesByQuery(query: string = "") {
     // TODO: evaluar string vacio
+    if(!this.userLocation) throw Error('No hay userLocation');
     this.isLoadingPlaces = true;
-    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?limit=5&language=es&access_token=pk.eyJ1IjoiY3Jpc3RpYW4wMTIiLCJhIjoiY2xyb2NubWE2MWpsMDJqa2k3M3NzMGhsbyJ9.LO9_4zw5Rv7nP3f_IA6t2A`)
+    this.placesApi.get<PlacesResponse>(`/${query}.json`, {
+      params: {
+        proximity: this.userLocation?.join(','),
+      }
+    })
       .subscribe(resp => {
         console.log(resp.features);
         this.places = resp.features;
