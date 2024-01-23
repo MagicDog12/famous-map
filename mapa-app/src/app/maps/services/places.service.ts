ae56173b00cc0a1130f9b9e1414b0655d39acf9f
Cristian <creature.secret@gmail.com>
2024-01-23 14:08:41 -0300

    add fitBounds to markers from places

--- mapa-app/src/app/maps/services/places.service.ts
+++ mapa-app/src/app/maps/services/places.service.ts
@@ -57,7 +57,7 @@ export class PlacesService {
     })
       .subscribe(resp => {
         this.places = resp.features;
-        this.mapService.createMarkersFromPlaces(this.places);
+        this.mapService.createMarkersFromPlaces(this.places, this.userLocation!);
         this.isLoadingPlaces = false;
       });
   }
